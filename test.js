2aebaf87196d73e8e62dad21eb5bd38b4b7ad018
Agrit Tiwari <agrit1408@gmail.com>
2021-11-14 13:45:07 +0530

    nested describe to add more tests as sub-group

--- test.js
+++ test.js
@@ -1,21 +1,54 @@
-describe("pow", function() {
-
-    function makeTest(x)
+describe("pow", function ()
+{
+    describe("raises to the power 3", function ()
     {
-        let expected = x * x * x;
-        it(`${ x } in the power 3 is ${ expected }`, function ()
+        function makeTest(x)
         {
-            assert.equal(pow(x,3), expected)
-        })
-    }
-    for (let x = 1; x <= 5; x++){
-        makeTest(x)
-    }
+            let expected = x * x * x;
+            it(`${ x } in the power 3 is ${ expected }`, function ()
+            {
+                assert.equal(pow(x,3), expected)
+            })
+        }
+        for (let x = 1; x <= 5; x++){
+            makeTest(x)
+        }
+    })
+
+    //other tests here....
+})
+
+
+
+// describe("pow", function ()
+// {
+
+//     function makeTest(x)
+//     {
+//         let expected = x * x * x;
+//         it(`${ x } in the power 3 is ${ expected }`, function ()
+//         {
+//             assert.equal(pow(x,3), expected)
+//         })
+//     }
+//     for (let x = 1; x <= 5; x++){
+//         makeTest(x)
+//     }
   
-  });
+//   });
 
 
+//   describe("pow", function() {
 
+//     it("2 raised to power 3 is 8", function() {
+//       assert.equal(pow(2, 3), 8);
+//     });
+  
+//     it("3 raised to power 4 is 81", function() {
+//       assert.equal(pow(3, 4), 81);
+//     });
+  
+//   });
 
 
 
