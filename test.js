d4e4602c6fa59772d5e348d4bde6dd4470950d14
Agrit Tiwari <agrit1408@gmail.com>
2021-11-14 14:08:12 +0530

    before/after beforeEach/afterEaqch functions

--- test.js
+++ test.js
@@ -18,6 +18,18 @@ describe("pow", function ()
     //other tests here....
 })
 
+describe("test", function() {
+
+    before(() => alert("Testing started – before all tests"));
+    after(() => alert("Testing finished – after all tests"));
+  
+    beforeEach(() => alert("Before a test – enter a test"));
+    afterEach(() => alert("After a test – exit a test"));
+  
+    it('test 1', () => alert(1));
+    it('test 2', () => alert(2));
+  
+  });
 
 
 // describe("pow", function ()
